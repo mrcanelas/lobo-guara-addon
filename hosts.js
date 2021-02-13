1243347678ca32709374076796ae9d80c898d6eb
Victor Guerra Veloso <victorgvbh@gmail.com>
2021-02-13 23:26:12 +0000

    Github actions updated domains

--- hosts.js
+++
@@ -1,6 +0,0 @@
-function getUrls(id, season, episode) {
-    const url = `http://uganet.xyz/host04.php?b=tv/${id}/${season}/dub/${episode}.mp4`;
-    const url2 = `http://uganet.xyz/host06.php?b=thor/${id}/${season}/dub/${episode}.mp4`;
-    return { url, url2 };
-}
-module.exports = { getUrls };
