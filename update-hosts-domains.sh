b879e878f7c970f5860b3c4cd8237c09110a4bd2
Victor Guerra Veloso <victorgvbh@gmail.com>
2021-02-13 21:01:57 -0300

    Force ubuntu to version 20.04 and python to version 3

--- update-hosts-domains.sh
+++ update-hosts-domains.sh
@@ -1,4 +1,4 @@
-python ./update-hosts-domain.py > hosts.js
+python3 ./update-hosts-domain.py > hosts.js
 npm install beamup-cli -g
 npx beamup config a.baby-beamup.club $GITHUBUSERNAME
 npx beamup init lobo-guara-addon
