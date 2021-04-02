8c3461168d2012be7b767def4cfedc4bebbd5211
Silas Alves <sialsan@hotmail.com>
2021-04-02 20:06:13 -0300

    Update hosts.js

--- hosts.js
+++ hosts.js
@@ -1,6 +1,6 @@
 function getUrls(id, season, episode) {
     const url = `http://pixconection.xyz/host04.php?b=tv/${id}/${season}/dub/${episode}.mp4`;
-    const url2 = `http://pixconection.xyz/host06.php?b=thor/${id}/${season}/dub/${episode}.mp4`;
+    const url2 = `http://uganet.xyz/host06.php?b=thor/${id}/${season}/dub/${episode}.mp4`;
     return { url, url2 };
 }
 module.exports = { getUrls };
